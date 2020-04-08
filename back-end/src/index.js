0dd132bda893a98378528061af7cb706f07da77b
Fernando Brozingamoraemoraeddddasafs-zxc <fbrozinga@outlook.coma1234>
2020-04-08 17:01:39 -0300

    modificaçao no drop da migration de ongs

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -2,10 +2,19 @@ const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
 
+const client = require('prom-client')
+const collectDefaultMetrics = client.collectDefaultMetrics
+collectDefaultMetrics({ timeout: 1000 })
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.get('/metrics', (req, res) => {
+    res.set('Content-Type', client.register.contentType)
+    res.end(client.register.metrics())
+  })
 
 app.listen(3333);
